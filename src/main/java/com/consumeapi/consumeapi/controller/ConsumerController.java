748e6cb088d29e1c70656f2771f640ad2f1531b9
anshuldevmehta <anshul.dev.mehta@outlook.in>
2024-01-11 20:13:50 +0530

    Added filtering.

--- src/main/java/com/consumeapi/consumeapi/controller/ConsumerController.java
+++ src/main/java/com/consumeapi/consumeapi/controller/ConsumerController.java
@@ -2,8 +2,10 @@ package com.consumeapi.consumeapi.controller;
 
 import com.consumeapi.consumeapi.domain.CreditCard;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -23,17 +28,47 @@ public class ConsumerController {
     {
         return "OK";
     }
+    private final String DATE_FORMAT="MM/YY";
+
+    private final String BASE_URL="https://fakerapi.it/api/v1/credit_cards?_quantity=";
+
     @GetMapping("consume")
-    public ResponseEntity<List<CreditCard>> consumeApi(@RequestParam final String URL)
+    public ResponseEntity<List<CreditCard>> consumeApi(@RequestParam final String thresholdDate,@RequestParam final String quantity)
     {
+
+        Date recievedThresholdDate=null;
+        Integer numberOfRecords=null;
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+        if(StringUtils.isNumeric(quantity))
+        {
+            numberOfRecords=Integer.parseInt(quantity);
+        }
+        else {
+            return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            recievedThresholdDate=sdf.parse(thresholdDate);
+
+        } catch (ParseException e) {
+            return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
+        }
         RestTemplate template= new RestTemplate();
-        String responseEntity=template.getForObject(URL, String.class);
-        Gson gson = new Gson();
+        String responseEntity=template.getForObject(BASE_URL+quantity, String.class);
+        Gson gson = new GsonBuilder()
+                .setDateFormat(DATE_FORMAT)
+                .create();
         JsonObject jSon = new Gson().fromJson(responseEntity, JsonObject.class);
         String creditCardJsonArray=jSon.get("data").toString();
 
         Type listType = new TypeToken<List<CreditCard>>() {}.getType();
         List<CreditCard> creditCardList = gson.fromJson(creditCardJsonArray, listType);
-        return new ResponseEntity<List<CreditCard>>(creditCardList, HttpStatus.OK);
+        Date finalRecievedThresholdDate = recievedThresholdDate;
+        creditCardList=creditCardList.stream().filter(creditCard -> {
+                return creditCard.getExpiration().after(finalRecievedThresholdDate);
+        }).toList();
+        return new ResponseEntity<>(creditCardList, HttpStatus.OK);
     }
+
+
 }
--- src/main/java/com/consumeapi/consumeapi/domain/CreditCard.java
+++ src/main/java/com/consumeapi/consumeapi/domain/CreditCard.java
@@ -3,12 +3,14 @@ package com.consumeapi.consumeapi.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Setter
 @Getter
 public class CreditCard
 {
     String type;
     String number;
-    String expiration;
+    Date expiration;
     String owner;
 }
