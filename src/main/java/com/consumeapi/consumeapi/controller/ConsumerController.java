bc67ec254588dcf173479068068500be333dee7f
anshuldevmehta <anshul.dev.mehta@outlook.in>
2024-01-11 18:20:56 +0530

    Updated to return list of Objects as JSON

--- src/main/java/com/consumeapi/consumeapi/controller/ConsumerController.java
+++ src/main/java/com/consumeapi/consumeapi/controller/ConsumerController.java
@@ -1,9 +1,9 @@
 package com.consumeapi.consumeapi.controller;
 
 import com.consumeapi.consumeapi.domain.CreditCard;
-import com.consumeapi.consumeapi.domain.CreditCardList;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
 @RestController
@@ -23,10 +24,16 @@ public class ConsumerController {
         return "OK";
     }
     @GetMapping("consume")
-    public ResponseEntity<String> consumeApi(@RequestParam final String URL)
+    public ResponseEntity<List<CreditCard>> consumeApi(@RequestParam final String URL)
     {
         RestTemplate template= new RestTemplate();
         String responseEntity=template.getForObject(URL, String.class);
-        return new ResponseEntity<String>(responseEntity, HttpStatus.OK);
+        Gson gson = new Gson();
+        JsonObject jSon = new Gson().fromJson(responseEntity, JsonObject.class);
+        String creditCardJsonArray=jSon.get("data").toString();
+
+        Type listType = new TypeToken<List<CreditCard>>() {}.getType();
+        List<CreditCard> creditCardList = gson.fromJson(creditCardJsonArray, listType);
+        return new ResponseEntity<List<CreditCard>>(creditCardList, HttpStatus.OK);
     }
 }
